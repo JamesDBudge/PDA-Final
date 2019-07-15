8ab303675cbdeae1fb21a2299cb69e4f28ce0402
James Budge <thebudgie@gmail.com>
2019-07-15 17:32:18 +0100

    up to week 7

--- hash.rb
+++ hash.rb
@@ -1,6 +1,37 @@
-hash = { :name => "James",
-         :age => "33",
-         :occupation => "Student"
-        }
+users = {
+  "Budgie" => {
+    :first_name => "James",
+    :last_name => "Budge",
+    :age => 33,
+    :occupation => "Student",
+    :favourite_things => ["PC Games", "Beer"]
+  },
 
-p hash[:name] + " is "+ hash[:age] + " years old, and is currently a "+ hash[:occupation]
+  "Jon" => {
+    :first_name => "Jonathon",
+    :last_name => "Tomaney",
+    :age => 27,
+    :occupation => "Student",
+    :favourite_things => ["Warhammer", "IPA"]
+  },
+
+  "Diego" => {
+    :first_name => "Diego",
+    :last_name => "Diniz",
+    :age => 28,
+    :occupation => "Student",
+    :favourite_things => ["F1 Racing", "Sunshine"]
+  }
+}
+
+# adding a person to the hash of users
+users[:Alex] ={
+  :first_name => "Alex",
+  :last_name => "Bazlinton",
+  :age => 35,
+  :occupation => "Lead Instructor",
+  :favourite_things => ["Javascript", "Variable Names"]
+}
+
+# printing the whole hash to see if the user was added
+p users
